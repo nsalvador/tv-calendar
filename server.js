73cb05d1c84ebdde884239c03621fbe93ce40d4d
Nigel Salvador <nigelsalvador@gmail.com>
2019-01-07 22:35:18 -0500

    Commented out line 1

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('./config');
+// require('./config');
 
 const express = require("express");
 const app = express();
@@ -21,19 +21,19 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-axios.defaults.baseURL = "https://api.thetvdb.com";
+// axios.defaults.baseURL = "https://api.thetvdb.com";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 function getSource(item) {
   return new Promise(async (resolve, reject) => {
